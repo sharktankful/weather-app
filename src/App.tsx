152da2e2e9a8cc4f7cbebc8a8ec2a47f631fa996
Alec Smith <alecmsmith18@gmail.com>
2023-06-10 16:54:15 -0500

    Built Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-
-  return <Button colorScheme='blue'>Button</Button>
-   
+  return (
+    <Grid templateAreas={{
+      base: `"nav" "city" "temp" "sky"`,
+      lg: `"nav city" "temp sky"` //1024px
+    }}>
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <GridItem area={"city"} bg={"gold"}>
+        City
+      </GridItem>
+      <GridItem area={"temp"} bg={"dodgerblue"}>
+        Temp
+      </GridItem>
+      <GridItem area={"sky"} bg={"green"}>
+        Sky
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
